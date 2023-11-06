0bab9621904267c80ba9e0d40e4dc1c8264c6578
nikfarjam <mm.nikfarjam@gmail.com>
2023-11-06 11:11:12 +0000

    format

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "postgres_port" {
-    type = number
-    default = 5432
+  type    = number
+  default = 5432
 }
 
 variable "hasura_port" {
-    type = number
-    default = 8090
+  type    = number
+  default = 8090
 }
 
 variable "POSTGRES_PASSWORD" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "HASURA_ADMIN_SECRET" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
